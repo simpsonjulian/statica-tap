f0e159ede8cd5c3ced9d1c764752633949575e83
Julian Simpson <julian@safeadvisory.co.nz>
2024-10-18 11:20:42 +1300

    Fix syntax of install

--- Formula/statica.rb
+++ Formula/statica.rb
@@ -19,7 +19,7 @@ class Statica < Formula
   def install
     bin.install 'statica'
     libexec.install %w[tools.d html_report.rb template.erb]
-    libexec.install 'csv2sarif.rb', 'csv2sarif'
+    libexec.install 'csv2sarif.rb' => 'csv2sarif'
   end
 
   test do
